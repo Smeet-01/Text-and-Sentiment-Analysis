1a81318dcfebd7cc12baaf320aa1340d79280c84
Smeet-01 <90793347+Smeet-01@users.noreply.github.com>
2023-04-30 19:01:34 +0530

    code readability and reusability improved

--- Metrics_for_Analysis.py
+++ Metrics_for_Analysis.py
@@ -1,196 +1,88 @@
-import List_of_words as lw
 from nltk.tokenize import wordpunct_tokenize
 from nltk.tokenize import sent_tokenize
 from nltk.tokenize import SyllableTokenizer
-import re
-import pandas as pd
-
-pos_score_ls =[]
-neg_score_ls =[]
-polar_score_ls =[]
-sub_score_ls =[]
-avg_sent_len_ls =[]
-per_complex_words_ls =[]
-fog_index_ls =[]
-avg_word_per_sent_ls =[]
-comp_word_count_ls =[]
-avg_syll_ls =[]
-pronoun_ls =[]
-avg_word_len_ls =[]
-word_count_ls =[]
 
 
 
-def Metrics(file):
-    # We open the txt file.
-    textfile1 = open(file, 'r',encoding='mbcs',errors='ignore')
-    textfile2 = []
-    textfile2 = textfile1.read()
-    textfile1.close()
-    
-    # We make a list of the positive and negative words 
-    positive_words = lw.positive_words.split()
-    negative_words = lw.negative_words.split()
-    
-    # We tokenize words from the text we obtained from the txt file and eliminate punctuation signs and symbols
-    words = wordpunct_tokenize(textfile2)
-    words1 = [word for word in words if word.isalpha()]
-    
-    # We tokenize the sentences similarly
-    sentences = sent_tokenize(textfile2, language='english')
-    
-    # We eliminate stop words 
-    lst = []
-    for word in words1:
-        if word in lw.auditor_stopwords.split():
-            pass
-        elif word in lw.currency_stopwords.split():
-            pass
-        elif word in lw.datetime_stopwords.split():
-            pass
-        elif word in lw.generic_stopwords.split():
-            pass 
-        elif word in lw.geographic_stopwords.split():
-            pass
-        elif word in lw.long_generic_stopwords.split():
-            pass
-        elif word in  lw.name_stopwords.split():
-            pass
-        else:
-            lst.append(word)
-            
-    # Function to calculate positivity score   
-    positivity_score = 0
-    positivity_score_list = []
-    for i in lst:
-        if i in positive_words:
-            positivity_score += 1
-            positivity_score_list.append(i)
-        else:
-            pass
+class Metrics:
 
-    # Function to calculate negativity score
-    negativity_score = 0 
-    negativity_score_list = []
-    for i in lst:
-        if i in negative_words:
-            negativity_score += 1
-            negativity_score_list.append(i)
-        else:
-            pass
+    #Pass text to be analyzed to the Instance. Text is broken up in two lists, words and sentences for analysis.
+    def __init__(self, Text):
+        words = wordpunct_tokenize(Text)
+        self.textwords = [word.lower() for word in words if word.isalpha()]
+        sentences = sent_tokenize(Text, 'english')
+        self.sentences = [sent for sent in sentences]
+        self.avg_sentence_length = round(len(self.textwords)/len(self.sentences),4)
         
-    # Polarity score 
-    polarity_score = round((positivity_score - negativity_score)/((positivity_score + negativity_score) + 0.000001),4)
+    #Positivity Score
+    def positivity_score(self, positivityList):
+        positivity_score = 0
+        for i in self.textwords:
+            if i in positivityList:
+                positivity_score += 1
+            else:
+                pass
+        return positivity_score
     
-    # Subjectivity score
-    subjectivity_score = round((positivity_score + negativity_score)/(len(lst) + 0.000001), 4)
+    #Negativity Score
+    def negativity_score(self, negativityList):
+        negativity_score = 0
+        for i in self.textwords:
+            if i in negativityList:
+                negativity_score += 1
+            else:
+                pass
+        return negativity_score
     
-    # Average sentence length
-    avg_sentence_length = round(len(words1)/len(sentences),4)
+    #Polarity Score  - ranges between -1 to 1
+    def polarity_score(self, positivity_score = int, negativity_score = int):
+        return round((positivity_score - negativity_score)/((positivity_score + negativity_score) + 0.000001),4)
     
-    # Percentage of complex words i.e., words with more than two syllables
-    SSP = SyllableTokenizer()
-    complex_words = []
-    for token in words1:
-        SSP.tokenize(token)
-        if (len(SSP.tokenize(token))) > 2:
-            complex_words.append(token)
+    #Subjectivity Score - ranges from 0 to 1
+    def subjectivity_score(self, positivity_score = int, negativity_score = int):
+        return round((positivity_score + negativity_score)/(len(self.textwords) + 0.000001), 4)
+    
+    #Complexity of text - percentage of words in the text that have three or more syllables
+    def text_complexity(self):
+        SSP = SyllableTokenizer()
+        complex_words = []
+        for token in self.textwords:
+            SSP.tokenize(token)
+            if (len(SSP.tokenize(token))) > 2:
+                complex_words.append(token)
+        comp_words_pcent = round((len(complex_words)/len(self.textwords)) * 100, 4)
+        return comp_words_pcent
 
-    percentage_of_complex_words = round((len(complex_words)/len(words1)) * 100, 4)
-    
-    # Gunning fog index
-    fog_index = round(0.4 * (avg_sentence_length + percentage_of_complex_words),4)
-    
-    # Average number of words in a sentence
-    avg_word_per_sent = round(len(words1)/len(sentences),4)
-    
-    # Count of complex words
-    complex_word_count = len(complex_words)
-    
-    # Count of all words
-    word_count = len(words1)
-    
-    # Syllable count for every word in the text. The commented code creates a dictionary where the key:value pair represents the word and its syllable count respectively.
-    syllable_count_per_word = [(len(SSP.tokenize(token)))for token in words1]
-    # res = {}
-    # for word in words1:
-    #     for value in syllable_count_per_word:
-    #         res[word] = value
-    #         syllable_count_per_word.remove(value)
-    #         break  
 
-    # Average number of syllables per word
-    average_syllable_per_word = sum(syllable_count_per_word)/len(syllable_count_per_word)
+    """Gunning Fog Index - In linguistics, the Gunning fog index is a readability test for English writing. 
+    The index estimates the years of formal education a person needs to understand the text on the first reading. 
+    For instance, a fog index of 12 requires the reading level of a United States high school senior (around 18 years old). 
+    The test was developed in 1952 by Robert Gunning, an American businessman who had been involved in newspaper and textbook publishing
     
-    # Pronouns used in the text
-    words1_str = ' '.join(map(str, words1))
-    pronounRegex = re.compile(r'\b(I|we|me|my|ours|(?-i:us))\b',re.I)
-    pronouns = pronounRegex.findall(words1_str)
+    https://en.wikipedia.org/wiki/Gunning_fog_index
+    """
     
-    # Average word length
-    average_word_length = sum([len(i) for i in words1])/len([len(i) for i in words1])
+    def GFI(self, text_complexity):
+        return round(0.4 * int(self.avg_sentence_length + text_complexity),4)
+    
+
+    #Calculates the average number of syllables for words in the given text
+    def Average_syllable_count(self):
+        SSP = SyllableTokenizer()
+        syllable_count_per_word = [(len(SSP.tokenize(token)))for token in self.textwords]
+        return round((sum(syllable_count_per_word)/len(syllable_count_per_word)), 4)
+             
+
+    
+
+    
+
     
     
-    word_count_ls.append(word_count)
+
         
-    print(f"The positivity score is: {positivity_score}")
-    pos_score_ls.append(positivity_score)
-    
-    print(f"The negativity score is: {negativity_score}")
-    neg_score_ls.append(negativity_score)
-    
-    print(f"The polarity score is: {polarity_score}")
-    polar_score_ls.append(polarity_score)
-    
-    print(f"The subjectivity score is: {subjectivity_score}")
-    sub_score_ls.append(subjectivity_score)
-    
-    print(f"The average sentence length is: {avg_sentence_length}")
-    avg_sent_len_ls.append(avg_sentence_length)
-    
-    print(f"The percentage of complex words is: {percentage_of_complex_words} %")
-    per_complex_words_ls.append(percentage_of_complex_words)
-    
-    print(f"The Gunning fog index score is: {fog_index}")
-    fog_index_ls.append(fog_index)
-    
-    print(f"The average number of words in a sentence is: {avg_word_per_sent}")
-    avg_word_per_sent_ls.append(avg_word_per_sent)
-    
-    print(f"The count of complex words is: {complex_word_count}")
-    comp_word_count_ls.append(complex_word_count)
-
-    print(f"The average syllable count per word is: {average_syllable_per_word}")
-    avg_syll_ls.append(average_syllable_per_word)
-    
-    print(f"The pronouns used in the text are: {pronouns}")
-    pronoun_ls.append(pronouns)
-
-    print(f"The average length of words expressed in characters is: {average_word_length}")
-    avg_word_len_ls.append(average_word_length)
-  
-
-output_files = []
-for i in range(1,151):
-    output_files.append(f'Outputs/{i}.txt')
 
 
-c = 1
-
-for i in output_files:
-    Metrics(i)
-    print('\n')
-    c +=1
-    print(c)
 
 
-df = pd.read_excel('Output_Data_Structure-Copy.xlsx')
-
-
-df.insert(14, "AVG WORD LENGTH", avg_word_len_ls)
-
-file_name = 'Output_Data_Structure-1.xlsx'
-  
-# saving the excel
-df.to_excel(file_name)
 
