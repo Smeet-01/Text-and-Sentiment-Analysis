795df209d49920e299bda2e113ed4ec7eaa89da0
Smeet Almeida <90793347+Smeet-01@users.noreply.github.com>
2023-05-01 10:54:13 +0530

    Update Metrics_for_Analysis.py
    Signed-off-by: Smeet Almeida <90793347+Smeet-01@users.noreply.github.com>

--- Metrics_for_Analysis.py
+++ Metrics_for_Analysis.py
@@ -54,7 +54,8 @@ class Metrics:
         return comp_words_pcent
 
 
-    """Gunning Fog Index - In linguistics, the Gunning fog index is a readability test for English writing. 
+    """
+    Gunning Fog Index - In linguistics, the Gunning fog index is a readability test for English writing. 
     The index estimates the years of formal education a person needs to understand the text on the first reading. 
     For instance, a fog index of 12 requires the reading level of a United States high school senior (around 18 years old). 
     The test was developed in 1952 by Robert Gunning, an American businessman who had been involved in newspaper and textbook publishing
